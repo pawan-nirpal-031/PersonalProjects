465bf2c515d323a9e14cc691000a71c6b597ccf0
Pawan Nirpal <pawannirpal@gmail.com>
2024-01-14 12:12:36 +0530

    sync4

--- gitPush.sh
+++ gitPush.sh
@@ -1,7 +1,3 @@
 git add . 
 git commit -m $1
-# git push origin main
-# pawan-nirpal-031
-# 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
-
 git push https://pawan-nirpal-031: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@github.com/pawan-nirpal-031/PersonalProjects.git main
