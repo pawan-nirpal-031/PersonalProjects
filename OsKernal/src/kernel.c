d7859a74ac77c3915268c4a1969a834996008228
Pawan Nirpal <pawannirpal@gmail.com>
2025-01-11 18:43:37 +0530

    [Kernel] - Printing hello world

--- OsKernal/src/kernel.c
+++ OsKernal/src/kernel.c
@@ -1,10 +1,57 @@
 #include "kernel.h"
+#include <stddef.h> // size_t
+#include <stdint.h>
 
-void kernelMain(){
-    char * videoMem = (char *) 0xb8000;
-    
-    for(int i =0; i<11; i+=2){
-        videoMem[i] = (char)(i+65); // Byte 1 is the character
-        videoMem[i+1] = 2; // Byte 2 is the color
+uint16_t *videoMem = 0;
+uint16_t TermRow = 0;
+uint16_t TermCol = 0;
+
+uint16_t terminalMakeChar(char c, char col) {
+  return  col << 8 | c;
+}
+
+void terminalPutChar(char c, char col, int x, int y) {
+  videoMem[(y * VGA_WIDTH) + x] = terminalMakeChar(c, col);
+}
+
+void terminalWriteChar(char c,char col){
+    if(c=='\n'){
+        TermCol = 0;
+        TermRow+=1;
+        return;
     }
+    terminalPutChar(c,col,TermCol,TermRow);
+    TermCol+=1;
+    if(TermCol >= VGA_WIDTH){
+        TermCol = 0;
+        TermRow+=1;
+    }
+}
+
+// Initialize the terminal with blank spaces
+void terminalInitalize() {
+  videoMem = (uint16_t *)0xB8000;
+  TermCol = 0;
+  TermRow = 0;
+  for (int y = 0; y < VGA_HEIGHT; y++)
+    for (int x = 0; x < VGA_WIDTH; x++)
+      terminalPutChar(' ', 0, x, y);
+}
+
+size_t strlen(const char *str) {
+  size_t len = 0;
+  while (str[len])
+    len++;
+  return len;
+}
+
+void print(const char *str) {
+  size_t len = strlen(str);
+  for (size_t i = 0; i < len; i++)
+    terminalWriteChar(str[i], 15);
+}
+
+void kernelMain() { 
+    terminalInitalize(); 
+    print("Hello, World!\nTest");
 }
