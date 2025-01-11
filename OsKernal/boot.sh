3dcff62f622fc4a0184c24dd65bfc88d5fbb5c2d
Pawan Nirpal <pawannirpal@gmail.com>
2025-01-11 17:36:21 +0530

    [Kernel] - Writing chars to video output

--- OsKernal/boot.sh
+++
@@ -1,3 +0,0 @@
-nasm -f bin ./src/boot/boot.asm -o ./bin.bin
-# ndisasm ./boot.bin 
-qemu-system-x86_64 -hda ./bin/boot.bin
--- OsKernal/src/kernel.c
+++ OsKernal/src/kernel.c
@@ -1,5 +1,10 @@
 #include "kernel.h"
 
 void kernelMain(){
-
+    char * videoMem = (char *) 0xb8000;
+    
+    for(int i =0; i<11; i+=2){
+        videoMem[i] = (char)(i+65); // Byte 1 is the character
+        videoMem[i+1] = 2; // Byte 2 is the color
+    }
 }
